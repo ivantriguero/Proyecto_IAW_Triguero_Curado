bdbcf5c0e71ef0487b28e16210bea5b9780ae8ff
usuario <you@example.com>
2019-01-14 13:43:42 +0100

    14/01/19 v2

--- registro/registro.php
+++ registro/registro.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MercaPalacio</title>
-    <link rel="shortcut icon" href="logo1.png" />
-    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="shortcut icon" href="../logo1.png" />
+    <link rel="stylesheet" type="text/css" href="registro.css">
     <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
     <style>
       span {
@@ -17,20 +17,13 @@
   <body>
       <div id="contenedor">
         <div id="cabecera">
-          <div id="usuario">
-            <span><a href="./registro/registro.php">Registrarse</a></span>
-            <span>Iniciar sesión</span>
-          </div>
-          <img id="logo" src="logo1.png">
+          <img id="logo" src="../logo1.png">
           <h1>MercaPalacio</h1>
           <div>Atención al cliente</div>
           <div>Proveedores</div>
           <div>Productos</div>
         </div>
         <div id="body"></div>
-        <div id="pie">
-        © - MercaPalacio<br>Iván Triguero Curado - IES Triana
-        </div>
       </div>
 
 
