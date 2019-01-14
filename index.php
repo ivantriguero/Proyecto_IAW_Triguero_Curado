70e76e38f9e95cfd160d7635f8ffb94f0bb680d9
ivantriguero <32703774+ivantriguero@users.noreply.github.com>
2019-01-14 09:59:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Passing info with POST and HTML FORMS using a single file.</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <style>
+      span {
+        width: 100px;
+        display: inline-block;
+      }
+    </style>
+  </head>
+  <body>
+      <div id="contenedor">
+      <div id="cabecera"></div>
+        <img src="logo1.png">
+
+      </div>
+
+
+
+      <?php 
+
+?>
+          
+
+  </body>
+</html>
