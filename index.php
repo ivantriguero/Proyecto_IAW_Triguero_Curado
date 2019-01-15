98d01be0893ea39837f39e43cc61b059a3ffe43e
usuario <you@example.com>
2019-01-15 12:53:57 +0100

    15/01/2019

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <link rel="shortcut icon" href="logo1.png" />
     <link rel="stylesheet" type="text/css" href="index.css">
     <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <style>
       span {
         width: 100px;
@@ -15,22 +17,20 @@
     </style>
   </head>
   <body>
-      <div id="contenedor">
-        <div id="cabecera">
-          <div id="usuario">
-            <span><a href="./registro/registro.php">Registrarse</a></span>
-            <span>Iniciar sesión</span>
+      <div class ="container-fluid" id="contenedor">
+          <div id="cabecera" class="row">
+            <div id="usuario" class="row align-content-right">
+              <div class="col-md-1">Iniciar sesión</div>
+              <div class="col-md-1">Registrase</div>
+            </div>
+            <div class="row" style="width:80%">
+              <div class="col-md-1"><img src="logo1.png" class="img-fluid"></div>
+              <div class="col-md-1"><h1>MercaPalacio</h1></div>
+              <div class="col-md-10">
+                <div><h2>Productos</h2></div>
+              </div>
+            </div>
           </div>
-          <img id="logo" src="logo1.png">
-          <h1>MercaPalacio</h1>
-          <div>Atención al cliente</div>
-          <div>Proveedores</div>
-          <div>Productos</div>
-        </div>
-        <div id="body"></div>
-        <div id="pie">
-        © - MercaPalacio<br>Iván Triguero Curado - IES Triana
-        </div>
       </div>
 
 
